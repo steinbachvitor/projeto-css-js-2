b1bd164b39a6b6a3a0c482a17eef8d60d001297d
Vitor Steinbach <131713625+steinbachvitor@users.noreply.github.com>
2024-05-29 09:16:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("messageButton").addEventListener('click', function(){
+    var messageParagraph = document.getElementById("message")
+    if (messageParagraph.classList.contains('hidden')) {
+    messageParagraph.classList.remove('hidden')
+    } else {
+    messageParagraph.classList.add('hidden')}
+    })
